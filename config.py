22e1500f1a16a34bcc4aedad7a3591a93dda8b8f
shr1k <110760070+schr1k@users.noreply.github.com>
2023-11-02 16:22:20 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,21 @@
+# Telegram
+TOKEN = 'token from BotFather'
+BUGS_GROUP_ID = 'group id'
+IDEAS_GROUP_ID = 'group id'
+RETURN_URL = 'link to your bot'
+
+# Postgres
+POSTGRES_USER = 'postgres'
+POSTGRES_PASSWORD = 'postgres'
+POSTGRES_DB = 'AnonChat'
+POSTGRES_HOST = 'localhost'
+POSTGRES_PORT = '5432'
+
+# Redis
+REDIS_DB = '0'
+REDIS_HOST = 'localhost'
+REDIS_PORT = '6379'
+
+# Yookassa
+YOOKASSA_ACCOUNT_ID = 'yookassa shop id'
+YOOKASSA_SECRET_KEY = 'yookassa api token'
